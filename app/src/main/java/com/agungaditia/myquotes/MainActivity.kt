8dfe5853731bc7154c9b4c2aa7b0b01bb47d71da
AgungAditia <agungaditia62@gmail.com>
2024-04-02 19:44:23 +0700

    Initial commit

---
+++ app/src/main/java/com/agungaditia/myquotes/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.agungaditia.myquotes
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.agungaditia.myquotes.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        getRandomQuotes()
+    }
+
+    private fun getRandomQuotes() {
+        // Menggunakan LoopJ untuk mengambil data dari API
+        binding.progressBar.visibility = View.VISIBLE
+
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/random"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                heades: Array<Header>,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
